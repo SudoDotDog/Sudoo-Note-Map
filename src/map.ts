d970f39fbc0370e4274ba38d0cf805b95de2b118
WMXPY <wm@mengw.io>
2020-02-06 12:03:46 -0600

    add remove function

--- src/map.ts
+++ src/map.ts
@@ -13,13 +13,18 @@ export class NoteMap<T extends any> {
         return new NoteMap<T>();
     }
 
+    // Identifier => Key
+    private readonly _reverseMap: Map<string, string>;
+    // Key => Identifier[]
     private readonly _supportMap: Map<string, string[]>;
-    private readonly _identifierMap: Map<string, T>;
+    // Identifier => Instance
+    private readonly _instanceMap: Map<string, T>;
 
     private constructor() {
 
+        this._reverseMap = new Map<string, string>();
         this._supportMap = new Map<string, string[]>();
-        this._identifierMap = new Map<string, T>();
+        this._instanceMap = new Map<string, T>();
     }
 
     public setAndGetIdentifer(key: string, value: T): string {
@@ -30,14 +35,34 @@ export class NoteMap<T extends any> {
         return identifier;
     }
 
-    public get(key: string): T[] {
+    public getInstances(key: string): T[] {
 
         const identifers: string[] = this._getIdentifiers(key);
         return this._getInstances(identifers);
     }
 
+    public removeByIdentifier(identifier: string): boolean {
+
+        const removeIdentifierResult: boolean = this._removeIdentifier(identifier);
+        if (!removeIdentifierResult) {
+            return false;
+        }
+        return this._removeInstance(identifier);
+    }
+
+    // SupportMap & ReverseMap
+    private _getIdentifiers(key: string): string[] {
+
+        const identifers: string[] | undefined = this._supportMap.get(key);
+        return identifers ?? [];
+    }
+
     private _addSupport(key: string, identifier: string): this {
 
+        // ReverseMap
+        this._reverseMap.set(identifier, key);
+
+        // SupportMap
         if (this._supportMap.has(key)) {
             const list: string[] = this._supportMap.get(key);
             this._supportMap.set(key, [...list, identifier]);
@@ -47,28 +72,46 @@ export class NoteMap<T extends any> {
         return this;
     }
 
-    private _getIdentifiers(key: string): string[] {
+    private _removeIdentifier(identifier: string): boolean {
 
-        const identifers: string[] | undefined = this._supportMap.get(key);
-        return identifers ?? [];
+        // ReverseMap
+        const key: string | undefined = this._reverseMap.get(identifier);
+        if (!key) {
+            return false;
+        }
+        this._reverseMap.delete(identifier);
+
+        // SupportMap
+        const identifierList: string[] | undefined = this._supportMap.get(key);
+        if (!identifierList) {
+            return false;
+        }
+        this._supportMap.set(key, identifierList.filter((each: string) => each !== identifier));
+        return true;
     }
 
-    private _addInstance(identifier: string, value: T): this {
-
-        this._identifierMap.set(identifier, value);
-        return this;
-    }
-
+    // InstanceMap
     private _getInstances(identifers: string[]): T[] {
 
         const result: T[] = [];
         for (const identifer of identifers) {
-            const value: T | undefined = this._identifierMap.get(identifer);
+            const value: T | undefined = this._instanceMap.get(identifer);
             if (typeof value !== 'undefined') {
                 result.push(value);
             }
         }
 
         return result;
+    }
+
+    private _addInstance(identifier: string, value: T): this {
+
+        this._instanceMap.set(identifier, value);
+        return this;
+    }
+
+    private _removeInstance(identifier: string): boolean {
+
+        return this._instanceMap.delete(identifier);
     }
 }
