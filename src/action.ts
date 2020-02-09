f7bf80295c97356cd4d76bf3a670314e05bf13dd
WMXPY <wm@mengw.io>
2020-02-08 22:07:05 -0600

    init modify action

---
+++ src/action.ts
@@ -0,0 +1,5 @@
+/**
+ * @author WMXPY
+ * @namespace Note_Map
+ * @description Action
+ */
---
+++ src/declare.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Note_Map
+ * @description Declare
+ */
+
+export enum MODIFY_ACTION {
+
+}
